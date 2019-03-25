9acf243dd5d136d97b52f700f590762617c2ed03
Justin Fry <mrjustinfry@gmail.com>
2019-03-25 14:59:23 -0400

    wireframes

--- contact.php
+++ contact.php
@@ -3,3 +3,9 @@ if($_POST["message"]) {
     mail("mrjustinfry@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+</body>
+</html>
